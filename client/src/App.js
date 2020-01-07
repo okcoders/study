9823f734fde333a710e32649714576a216cdc7f1
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-01-07 14:56:30 -0600

    Basic table with input info working

--- client/src/App.js
+++ client/src/App.js
@@ -15,54 +15,24 @@ import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 
 const columns = [
-  { id: "name", label: "Name", minWidth: 170 },
-  { id: "commit", label: "Latest Commit Date", minWidth: 100 }
-  /*
+  { id: "name", label: "Name", align: "center" },
   {
-    id: "population",
-    label: "Population",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toLocaleString()
+    id: "numberCommits",
+    label: "Number of Commits",
+    format: value => value.toLocaleString(),
+    align: "center"
   },
   {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toLocaleString()
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: value => value.toFixed(2)
-  }*/
+    id: "commit",
+    label: "Latest Commit Date",
+    format: value => value.toLocaleString(),
+    align: "center"
+  }
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(name, numberCommits, commit) {
+  return { name, numberCommits, commit };
 }
-
-const rows = [
-  createData("India", "IN", 1324171354, 3287263),
-  createData("China", "CN", 1403500365, 9596961),
-  createData("Italy", "IT", 60483973, 301340),
-  createData("United States", "US", 327167434, 9833520),
-  createData("Canada", "CA", 37602103, 9984670),
-  createData("Australia", "AU", 25475400, 7692024),
-  createData("Germany", "DE", 83019200, 357578),
-  createData("Ireland", "IE", 4857000, 70273),
-  createData("Mexico", "MX", 126577691, 1972550),
-  createData("Japan", "JP", 126317000, 377973),
-  createData("France", "FR", 67022000, 640679),
-  createData("United Kingdom", "GB", 67545757, 242495),
-  createData("Russia", "RU", 146793744, 17098246),
-  createData("Nigeria", "NG", 200962417, 923768),
-  createData("Brazil", "BR", 210147125, 8515767)
-];
 
 const useStyles = makeStyles({
   root: {
@@ -81,6 +51,10 @@ function App() {
     });
   }, []);
 
+  const rows = userProfiles.map(p =>
+    createData(p.name || p.username, p.numberOfCommits, p.latestCommitDate)
+  );
+
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -97,10 +71,11 @@ function App() {
   return (
     <>
       <AppBar position="static">
-        <Toolbar>Study</Toolbar>
+        <Toolbar>OKCoders Bootcamp</Toolbar>
       </AppBar>
       <Container fixed maxWidth="xl">
         <div className="App">
+          <h2>Classlist:</h2>
           <ol>
             {userProfiles.map(p => (
               <li>{p.name || p.username}</li>
@@ -108,7 +83,6 @@ function App() {
           </ol>
         </div>
       </Container>
-
       <Paper className={classes.root}>
         <TableContainer className={classes.container}>
           <Table stickyHeader aria-label="sticky table">
